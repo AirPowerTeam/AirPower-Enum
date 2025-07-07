e7df7c8e47e12755b4fa374552aa88f8430a7e69
Hamm <majhamm@qq.com>
2025-07-07 15:48:39 +0800

    build(dependencies): 更新项目依赖并升级版本号
    - 将项目版本号从 0.1.0 升级到 1.2.0
    - 更新 @antfu/eslint-config 版本从 4.11.0到 4.16.2
    - 更新 @types/node 版本从 22.13.14 到 24.0.10
    - 更新 eslint 版本从 9.23.0 到 9.30.1
    - 更新 vite 版本从 6.2.0 到 6.3.4
    - 更新 vite-plugin-dts 版本从 4.5.3 到 4.5.4
    - 更新 typescript 版本从 5.8.2 到 5.8.3

--- src/enum/type.ts
+++ src/enum/type.ts
@@ -9,5 +9,8 @@ export type EnumKey = string | number | boolean
  * ### 枚举类
  */
 export type EnumConstructor<K extends EnumKey = number, E extends Enum<K> = Enum<K>> = {
+  /**
+   * ### 创建枚举实例
+   */
   new(...args: any[]): E
 } & typeof Enum<K>
